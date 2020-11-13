94ba935c7a6175787053c8a745bf50ba3e31b27a
jrbrad <james.bradley@mason.wm.edu>
2020-11-13 09:26:39 -0500

    Upload git_tutorial folder

---
+++ git_tutorial/my.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 26 14:32:21 2017
+
+@author: jrbrad
+"""
+
+x = raw_input('Please enter a number:')
+
+try:
+    x = float(x)
+except:
+    print('The input could not be interpreted as a numerical value')
+    
+print(x)       # This is a debugging statement
+
+y = x/2
+
+print(y)
